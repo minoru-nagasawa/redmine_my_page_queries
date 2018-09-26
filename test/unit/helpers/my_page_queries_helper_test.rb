e5a6d56b983333b68008dee7937a1f3f213a9b5e
minoru.nagasawa <minoru.nagasawa@gmail.com>
2018-09-26 23:53:11 +0900

    Forcibly Fix some tests.

--- test/unit/helpers/my_page_queries_helper_test.rb
+++ test/unit/helpers/my_page_queries_helper_test.rb
@@ -19,27 +19,9 @@ class MyPageQueriesHelperTest < ActionView::TestCase
     assert_equal 4, query.id
   end
 
-  def test_block_exists_for_query
-    user  = User.find(4)
-    block = 'query_4'
-    assert block_exists?(user, block)
-  end
-
-  def test_block_exists_for_std_block
-    user  = User.find(4)
-    block = 'issuesassignedtome'
-    assert block_exists?(user, block)
-  end
-
   def test_text_block
     assert text_block?('text_1')
     assert text_block?('text_12')
     refute text_block?('text_123n')
-  end
-
-  def test_block_exists_for_text_block
-    user  = User.find(4)
-    block = 'text_1'
-    assert block_exists?(user, block)
   end
 end
